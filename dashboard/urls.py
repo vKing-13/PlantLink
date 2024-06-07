015ba91ec678ac7b6775bf381001459ab3f121ff
Vico <vicolee1363@gmail.com>
2024-06-07 20:05:47 +0800

    PLAN-10 added public url

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     path('<str:channel_id>/share', views.share_channel, name="share_channel"),
     path('<str:channel_id>/get_dashboard_data/', views.getDashboardData, name="getDashboardData"),
     path('<str:channel_id>/get_shared_dashboard', views.getSharedDashboardDetail, name="getSharedDashboardDetail"),
-
+    path('<str:channel_id>/shared_dashboard/', views.sharedDashboard, name="sharedDashboard"),
+    
     path('<str:channel_id>/manage_sensor', views.manage_sensor, name="manage_sensor"),
 
     path('<str:channel_id>/edit_sensor/<str:sensor_type>/<str:sensor_id>/', views.edit_sensor, name="edit_sensor"),
