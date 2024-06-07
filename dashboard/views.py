1725a233270cb2396b163644774529f491ebc103
Vico <vicolee1363@gmail.com>
2024-06-07 18:45:19 +0800

    PLAN-22 PLAN-20 chart dynamically render

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,7 @@ import json
 from django.shortcuts import render, redirect
 from bson import ObjectId
 from django.http import HttpResponse, JsonResponse
+import requests
 from dashboard.forms import ChannelForm, SensorForm
 from main.mongo_setup import connect_to_mongodb
 from datetime import datetime
@@ -146,13 +147,8 @@ def view_channel_sensor(request, channel_id):
                 channel_name = channel.get('channel_name', '')
                 description = channel.get('description', '')
                 sensor = channel.get('sensor', '')
-
-                ph_values = []
-                timestamps = []
-                humid_values = []
-                temp_values = []
-                timestamps_humid_temp = []
                 API=""
+                graph_count=0
                 for datapoint in sensor:
                     if 'DHT_sensor' in datapoint:
                         dht = datapoint['DHT_sensor']
@@ -160,76 +156,21 @@ def view_channel_sensor(request, channel_id):
                         dht_id = ObjectId(dht)
                         humid_temp_data = collection_humid_temp.find_one({"_id": dht_id})
                         API=humid_temp_data.get("API_KEY",'')
-                        for data_point in humid_temp_data.get('sensor_data', []):
-                            humidity_value = data_point.get('humidity_value', '')
-                            temperature_value = data_point.get('temperature_value', '')
-
-                            # Append humidity value and temperature value to lists
-                            humid_values.append(humidity_value)
-                            temp_values.append(temperature_value)
-
-                            timestamp_obj = data_point.get('timestamp', datetime.utcnow())
-                            formatted_timestamp = timestamp_obj.astimezone(pytz.utc).strftime('%d-%m-%Y')
-                            timestamps_humid_temp.append(formatted_timestamp)
+                        graph_count+=2
                     if 'PH_sensor' in datapoint:
                         ph = datapoint['PH_sensor']
                         db_ph, collection_ph = connect_to_mongodb('sensor', 'PHSensor')
                         ph_id = ObjectId(ph)
                         ph_data = collection_ph.find_one({"_id": ph_id})
                         API=ph_data.get("API_KEY",'')
-                        if ph_data:
-                            for data_point in ph_data.get('sensor_data', []):
-                                ph_values.append(data_point.get('ph_value', ''))
-                                timestamp_obj = data_point.get('timestamp', datetime.utcnow())
-                                formatted_timestamp = timestamp_obj.astimezone(pytz.utc).strftime('%d-%m-%Y')
-                                timestamps.append(formatted_timestamp)
-                        else:
-                            print("No PH sensor data found for the given ID")
+                        graph_count+=1
                 context = {
                     "channel_name": channel_name,
                     "description": description,
                     "channel_id": channel_id,
-                    "ph_values": ph_values,
-                    "timestamps": timestamps,
-                    "humid_values": humid_values,
-                    "temp_values": temp_values,
-                    "timestamps_humid_temp": timestamps_humid_temp,
-                    "API":API
+                    "API":API,
+                    "graph_count":graph_count
                 }
-
-                print("before model")
-                # Load the trained Random Forest model
-                model = load_trained_model()
-
-                if model:
-                    # Prepare input data for model prediction
-                    input_data = {
-                        'N': 0,  # Provide dummy values for features not used in prediction
-                        'P': 0,
-                        'K': 0,
-                        'temperature': float(temp_values[-1]) if temp_values else 0.0,  # Example temperature value
-                        'humidity': float(humid_values[-1]) if humid_values else 0.0,  # Example humidity value
-                        'ph': float(ph_values[-1]) if ph_values else 0.0,  # Example pH value
-                        'rainfall': 120.0,  # Example rainfall value
-                    }
-
-                    input_df = pd.DataFrame([input_data])
-
-                    # Make predictions using the model
-                    prediction = model.predict(input_df)
-                    
-                    probabilities = model.predict_proba(input_df)
-                    
-                    labels = model.classes_
-
-                    # Combine the labels with their probabilities and sort them by probability in descending order
-                    crop_recommendations = [
-                        {"crop": label, "accuracy": prob * 100}  # Convert to percentage
-                        for label, prob in zip(labels, probabilities[0])
-                    ]
-                    crop_recommendations.sort(key=lambda x: x["accuracy"], reverse=True)
-                    # Add the crop recommendation to the context
-                    context["crop_recommendations"] = crop_recommendations
 
                 return render(request, 'dashboard.html', context)
             else:
@@ -239,6 +180,235 @@ def view_channel_sensor(request, channel_id):
         
     else:
         return redirect('logPlantFeed')
+
+def share_channel(request,channel_id):
+    _id=ObjectId(channel_id)
+    db, collection = connect_to_mongodb("channel","dashboard")
+    if db is not None and collection is not None:
+        channel=collection.find_one({"_id":_id})
+        if channel:
+            result = collection.update_one(
+                {"_id":_id},
+                {"$set":{"privacy": "public"}}
+                )
+            if result.modified_count>0:
+                plantfeed_link=""
+                channel_data = {
+                    "channel_id": _id,
+                    "embed_link": f"https://your-plantlink-domain.com/public/channel/{channel_id}"
+                }
+                response = requests.post(plantfeed_link,json=channel_data)
+                if response.status_code == 200:
+                    return redirect('')
+                else:
+                    return JsonResponse({"error":"Failed to share channel"},status=500)
+                    
+        else:
+            return JsonResponse({"success": False, "error": "Document not found"})
+    else:
+        print("Error connecting to MongoDB.")
+        
+def getDashboardData(request,channel_id):
+    _id = ObjectId(channel_id)
+    db, collection = connect_to_mongodb('Channel', 'dashboard')
+    if db is not None and collection is not None:
+        channel = collection.find_one({"_id": _id})
+
+        if channel:
+            sensor = channel.get('sensor', '')
+
+            ph_values = []
+            timestamps = []
+            humid_values = []
+            temp_values = []
+            timestamps_humid_temp = []
+            API=""
+            for datapoint in sensor:
+                if 'DHT_sensor' in datapoint:
+                    dht = datapoint['DHT_sensor']
+                    db_humid_temp, collection_humid_temp = connect_to_mongodb('sensor', 'DHT11')
+                    dht_id = ObjectId(dht)
+                    humid_temp_data = collection_humid_temp.find_one({"_id": dht_id})
+                    API=humid_temp_data.get("API_KEY",'')
+                    for data_point in humid_temp_data.get('sensor_data', []):
+                        humidity_value = data_point.get('humidity_value', '')
+                        temperature_value = data_point.get('temperature_value', '')
+
+                        # Append humidity value and temperature value to lists
+                        humid_values.append(humidity_value)
+                        temp_values.append(temperature_value)
+
+                        timestamp_obj = data_point.get('timestamp', datetime.utcnow())
+                        formatted_timestamp = timestamp_obj.astimezone(pytz.utc).strftime('%d-%m-%Y')
+                        timestamps_humid_temp.append(formatted_timestamp)
+                if 'PH_sensor' in datapoint:
+                    ph = datapoint['PH_sensor']
+                    db_ph, collection_ph = connect_to_mongodb('sensor', 'PHSensor')
+                    ph_id = ObjectId(ph)
+                    ph_data = collection_ph.find_one({"_id": ph_id})
+                    API=ph_data.get("API_KEY",'')
+                    if ph_data:
+                        for data_point in ph_data.get('sensor_data', []):
+                            ph_values.append(data_point.get('ph_value', ''))
+                            timestamp_obj = data_point.get('timestamp', datetime.utcnow())
+                            formatted_timestamp = timestamp_obj.astimezone(pytz.utc).strftime('%d-%m-%Y')
+                            timestamps.append(formatted_timestamp)
+                    else:
+                        print("No PH sensor data found for the given ID")
+            context = {
+                "channel_id": channel_id,
+                "ph_values": ph_values,
+                "timestamps": timestamps,
+                "humid_values": humid_values,
+                "temp_values": temp_values,
+                "timestamps_humid_temp": timestamps_humid_temp,
+                "API":API,
+            }
+
+            # Load the trained Random Forest model
+            model = load_trained_model()
+
+            if model:
+                # Prepare input data for model prediction
+                input_data = {
+                    'N': 0,  # Provide dummy values for features not used in prediction
+                    'P': 0,
+                    'K': 0,
+                    'temperature': float(temp_values[-1]) if temp_values else 0.0,  # Example temperature value
+                    'humidity': float(humid_values[-1]) if humid_values else 0.0,  # Example humidity value
+                    'ph': float(ph_values[-1]) if ph_values else 0.0,  # Example pH value
+                    'rainfall': 120.0,  # Example rainfall value
+                }
+
+                input_df = pd.DataFrame([input_data])
+
+                # Make predictions using the model
+                prediction = model.predict(input_df)
+                
+                probabilities = model.predict_proba(input_df)
+                
+                labels = model.classes_
+
+                # Combine the labels with their probabilities and sort them by probability in descending order
+                crop_recommendations = [
+                    {"crop": label, "accuracy": prob * 100}  # Convert to percentage
+                    for label, prob in zip(labels, probabilities[0])
+                ]
+                crop_recommendations.sort(key=lambda x: x["accuracy"], reverse=True)
+                # Add the crop recommendation to the context
+                context["crop_recommendations"] = crop_recommendations
+
+            return JsonResponse(context)
+        else:
+            return JsonResponse({"success": False, "error": "Document not found"})
+    else:
+        print("Error connecting to MongoDB.")
+
+def getSharedDashboardDetail(request,channel_id):
+    _id = ObjectId(channel_id)
+    db, collection = connect_to_mongodb('Channel', 'dashboard')
+    if db is not None and collection is not None:
+        channel = collection.find_one({"_id": _id})
+
+        if channel:
+            print("Found channel")
+            channel_name = channel.get('channel_name', '')
+            description = channel.get('description', '')
+            sensor = channel.get('sensor', '')
+
+            ph_values = []
+            timestamps = []
+            humid_values = []
+            temp_values = []
+            timestamps_humid_temp = []
+            API=""
+            graph_count=0
+            for datapoint in sensor:
+                if 'DHT_sensor' in datapoint:
+                    dht = datapoint['DHT_sensor']
+                    db_humid_temp, collection_humid_temp = connect_to_mongodb('sensor', 'DHT11')
+                    dht_id = ObjectId(dht)
+                    humid_temp_data = collection_humid_temp.find_one({"_id": dht_id})
+                    API=humid_temp_data.get("API_KEY",'')
+                    graph_count+=2
+                    for data_point in humid_temp_data.get('sensor_data', []):
+                        humidity_value = data_point.get('humidity_value', '')
+                        temperature_value = data_point.get('temperature_value', '')
+
+                        # Append humidity value and temperature value to lists
+                        humid_values.append(humidity_value)
+                        temp_values.append(temperature_value)
+
+                        timestamp_obj = data_point.get('timestamp', datetime.utcnow())
+                        formatted_timestamp = timestamp_obj.astimezone(pytz.utc).strftime('%d-%m-%Y')
+                        timestamps_humid_temp.append(formatted_timestamp)
+                if 'PH_sensor' in datapoint:
+                    ph = datapoint['PH_sensor']
+                    db_ph, collection_ph = connect_to_mongodb('sensor', 'PHSensor')
+                    ph_id = ObjectId(ph)
+                    ph_data = collection_ph.find_one({"_id": ph_id})
+                    API=ph_data.get("API_KEY",'')
+                    graph_count+=1
+                    if ph_data:
+                        for data_point in ph_data.get('sensor_data', []):
+                            ph_values.append(data_point.get('ph_value', ''))
+                            timestamp_obj = data_point.get('timestamp', datetime.utcnow())
+                            formatted_timestamp = timestamp_obj.astimezone(pytz.utc).strftime('%d-%m-%Y')
+                            timestamps.append(formatted_timestamp)
+                    else:
+                        print("No PH sensor data found for the given ID")
+            context = {
+                "channel_name": channel_name,
+                "description": description,
+                "channel_id": channel_id,
+                "ph_values": ph_values,
+                "timestamps": timestamps,
+                "humid_values": humid_values,
+                "temp_values": temp_values,
+                "timestamps_humid_temp": timestamps_humid_temp,
+                "API":API,
+                "graph_count":graph_count
+            }
+
+            print("before model")
+            # Load the trained Random Forest model
+            model = load_trained_model()
+
+            if model:
+                # Prepare input data for model prediction
+                input_data = {
+                    'N': 0,  # Provide dummy values for features not used in prediction
+                    'P': 0,
+                    'K': 0,
+                    'temperature': float(temp_values[-1]) if temp_values else 0.0,  # Example temperature value
+                    'humidity': float(humid_values[-1]) if humid_values else 0.0,  # Example humidity value
+                    'ph': float(ph_values[-1]) if ph_values else 0.0,  # Example pH value
+                    'rainfall': 120.0,  # Example rainfall value
+                }
+
+                input_df = pd.DataFrame([input_data])
+
+                # Make predictions using the model
+                prediction = model.predict(input_df)
+                
+                probabilities = model.predict_proba(input_df)
+                
+                labels = model.classes_
+
+                # Combine the labels with their probabilities and sort them by probability in descending order
+                crop_recommendations = [
+                    {"crop": label, "accuracy": prob * 100}  # Convert to percentage
+                    for label, prob in zip(labels, probabilities[0])
+                ]
+                crop_recommendations.sort(key=lambda x: x["accuracy"], reverse=True)
+                # Add the crop recommendation to the context
+                context["crop_recommendations"] = crop_recommendations
+
+            return JsonResponse(context)
+        else:
+            return JsonResponse({"success": False, "error": "Document not found"})
+    else:
+        print("Error connecting to MongoDB.")
 
 
 def delete_channel(request,channel_id):
