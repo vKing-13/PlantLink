e41ddbfb4384ef23ea00681f5e414835dbe8a95e
Vico <vicolee1363@gmail.com>
2024-06-12 20:35:17 +0800

    PLAN-10 reconfigure middleware

--- PlantLink/settings.py
+++ PlantLink/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'PlantLink.middleware.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'PlantLink.urls'
